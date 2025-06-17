2193136758fbd9b28aa01bc1a4a5c0225058bf65
Orion10 <82806110+Messi-Orion10@users.noreply.github.com>
2025-06-17 13:23:25 +0200

    Update 2022-work_experience-2.m

--- _work_experiences/2022-work_experience-2.m
+++ _work_experiences/2022-work_experience-2.m
@@ -1,1 +1,18 @@
+---
+title: "Tutoring Activity"
+collection: work_experiences
+type: "Tutoring"
+permalink: /work_experiences/2021-work_experience-1
+venue: "Sapienza University of Rome – Latina Campus"
+period: "October 2022 - January 2023"
+location: "Latina (LT)"
+---
+
+Tutor for the course **Fondamenti di Automatica I – Teoria dei Sistemi**, under the supervision of Prof. Mattia Mattioni.
+
+- Designed and explained integrative exercises covering the core topics of the course
+- Conducted weekly problem-solving sessions to support students’ understanding
+- Coordinated with the course instructor to track progress and adjust materials as needed
+
+The tutoring activity took place during the first semester of the 2022/2023 academic year at the Latina campus of Sapienza University of Rome.
 
